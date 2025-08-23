d14a44f162e9deb5119f98276277e3d9d0a54c9e
Muhammadali251018 <muhammadali251018@gmail.com>
2025-08-23 14:48:42 +0300

    Add runAllTests task

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,15 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+tasks.register("runAllTests") {
+    group = "verification"
+    description = "Runs all tests in the project."
+
+    subprojects.forEach { subproject ->
+        subproject.tasks.matching { it.name == "testDebugUnitTest" || it.name == "test" }.forEach { task ->
+            dependsOn(task)
+        }
+    }
+}
+
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
