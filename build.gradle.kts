df5acd9fd97e8caabe70b1e317e1e5bdb49f1aa6
Muhammadali251018 <muhammadali251018@gmail.com>
2025-08-23 14:50:18 +0300

    Add count kotlin lines in project

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,21 @@ tasks.register("runAllTests") {
     }
 }
 
+tasks.register("count_kotlin_lines") {
+    group = "reporting"
+    description = "Counts the number of lines of Kotlin code in the project."
+
+    doLast {
+        val kotlinFiles = fileTree(".") {
+            include("**/*.kt", "**/*.kts")
+            exclude("**/build/**", "**/.gradle/**", "**/node_modules/**")
+        }
+
+        val totalLines = kotlinFiles.sumOf { it.readLines().size }
+        println("Total lines of Kotlin code: $totalLines")
+    }
+}
+
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
