74e0bc2ed288181fddde57cf2dcd235c72939181
MuhammadAli251018 <muhammadali251018@gmail.com>
2025-08-26 22:38:51 +0300

    Fix Alias emv varialbe name

--- generate_release_bundle.sh
+++ generate_release_bundle.sh
@@ -15,7 +15,7 @@ mkdir -p ~/.gradle
 {
   echo "RELEASE_STORE_FILE=release.keystore"
   echo "RELEASE_STORE_PASSWORD=$RELEASE_STORE_PASSWORD"
-  echo "RELEASE_KEY_ALIAS=$KEY_ALIAS"
+  echo "RELEASE_KEY_ALIAS=$RELEASE_KEY_ALIAS"
   echo "RELEASE_KEY_PASSWORD=$RELEASE_KEY_PASSWORD"
   echo "VERSION_REF=$VERSION_REF"
 } >> ~/.gradle/gradle.properties
